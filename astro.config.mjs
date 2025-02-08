f19740453a4fbb70a2b0b63580ac9256fe0157d5
Arthur Rufino de Camargo,MBA <77815039+laplaceconsulting@users.noreply.github.com>
2025-02-08 00:51:02 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import node from '@astrojs/node';
 
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server',
+  output: 'static',
   adapter: node({
     mode: 'standalone'
   }),
