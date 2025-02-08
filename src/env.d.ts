dd3f3393f26b0b289cb896f36aad2d671f38a571
Arthur Rufino de Camargo,MBA <77815039+laplaceconsulting@users.noreply.github.com>
2025-02-08 01:37:56 -0300

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,8 +2,8 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-  readonly PUBLIC_SUPABASE_URL: string;
-  readonly PUBLIC_SUPABASE_ANON_KEY: string;
+  readonly UPABASE_URL: string;
+  readonly SUPABASE_ANON_KEY: string;
 }
 
 interface ImportMeta {
