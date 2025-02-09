aecf3df48a9c4aafa7ca94eb939e62c1cfd46deb
Arthur Rufino de Camargo,MBA <77815039+laplaceconsulting@users.noreply.github.com>
2025-02-09 09:14:40 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
-import node from '@astrojs/node';
+   import tailwind from '@astrojs/tailwind';
+   import react from '@astrojs/react';
+   import netlify from '@astrojs/netlify';
 
-export default defineConfig({
-  integrations: [tailwind(), react()],
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
-  vite: {
-    define: {
-      'process.env.SUPABASE_URL': JSON.stringify(process.env.PUBLIC_SUPABASE_URL),
-      'process.env.SUPABASE_ANON_KEY': JSON.stringify(process.env.PUBLIC_SUPABASE_ANON_KEY)
-    }
-  }
-});
+   export default defineConfig({
+     integrations: [tailwind(), react()],
+     output: 'server',
+     adapter: netlify({
+       edgeMiddleware: true
+     }),
+     vite: {
+       define: {
+         'process.env.SUPABASE_URL': JSON.stringify(process.env.PUBLIC_SUPABASE_URL),
+         'process.env.SUPABASE_ANON_KEY': JSON.stringify(process.env.PUBLIC_SUPABASE_ANON_KEY)
+       }
+     }
+   });
